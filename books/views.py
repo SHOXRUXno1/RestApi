8642efcb081d153fbf0e6aa93e1fdcbe640ece9b
SHOXRUXno1 <148218372+SHOXRUXno1@users.noreply.github.com>
2024-04-10 21:12:09 +0500

    viewlar yozildi

---
+++ books/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+from rest_framework.response import responses
+
+
+class BookListApiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailApiView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateApiView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateApiView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteApiView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
